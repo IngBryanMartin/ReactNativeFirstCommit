601f8b79a2c5d60c62afd0b8d32092e117cde13f
Bryan <ing.bryanmartin@gmail.com>
2025-01-13 19:35:24 -0500

    Cambios en el index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const name = 'Bryan Martin Quispe Lavalle'
 const age = 26
 console.log('Hola, JAVASCRIPT en VSCODE');
+
 console.log(name, age)
 console.log(`Hola, mi nombre es ${name} y tengo ${age} años`);
